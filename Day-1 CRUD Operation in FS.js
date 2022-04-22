5d77746e125269efcc8b258b4be05a4777e27fc6
Humza Sajid <pak786hamzasajid66@Gmail.com>
2022-04-22 18:20:17 +0500

    Your  message

--- Day-1 CRUD Operation in FS.js
+++ Day-1 CRUD Operation in FS.js
@@ -1,3 +1,7 @@
+//Day -1 CRUD Operations in NodeJS using File System 
+
+
+
 const fs = require('fs');
 
 //****1st TASK****
@@ -5,35 +9,36 @@ const fs = require('fs');
 //     console.log(error)
 // });
 
-//**2ND TASK */
+
+//**2ND TASK  || Create a file */
 // fs.writeFile("Hamza/bio.txt","Welcome to the Pakistan", (e)=>
 // {
 //     console.log(e);
 // }
 // )
 
-//**3rd Task */
+//**3rd Task   || Adding data in existing file*/
 // fs.appendFile("Hamza/bio.txt"," \n NEW DATA" , (e)=>
 // {
 //     console.log(e);
 // })
 
 
-//**4th task */
+//**4th task || Reading with UTF-8*/
 
 // fs.readFile("Hamza/bio.txt","utf8",(e,data)=>
 // {
 //     console.log(data);
 // })
 
-//*5th Task//
+//*5th Task ||Renaming a file//
 
 // fs.rename("Hamza/bio.txt","mybio.txt",(e)=>{
 //     console.log(e)
 // })
 
 
-//**6th Task */
+//**6th Task || Deleting a file */
 
 // fs.unlink("mybio.txt",(e)=>
 // {
@@ -41,7 +46,7 @@ const fs = require('fs');
 // })
 
 
-//**7th Task */
+//**7th Task || Deleting a director*/
 
 fs.rmdir("Hamza",(e)=>
 {
